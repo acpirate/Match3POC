929e9191b9948b7e14d12c2973eb76c5c929123e
acpirate <3apx3zss29@snkmail.com>
2015-05-11 16:57:02 -0400

    added parsing to console

--- Assets/Assets/Scripts/ConsoleController.cs
+++ Assets/Assets/Scripts/ConsoleController.cs
@@ -31,7 +31,8 @@ public class ConsoleController : MonoBehaviour {
 	void ConsoleCommand(string commandString)
 	{
 		consoleInput.text="";
-		ConsoleOutputAdd(commandString);
+		ConsoleOutputAdd(">" + commandString);
+		ConsoleCommandParse(commandString);
 		consoleInput.ActivateInputField();
 	}
 
@@ -40,7 +41,7 @@ public class ConsoleController : MonoBehaviour {
 
 
 		if (consoleOutput.text!="" && consoleOutput.text.Split('\n').Length<consoleLines+1) consoleOutput.text+="\n";
-		consoleOutput.text+=">"+outputToAdd;
+		consoleOutput.text+=outputToAdd;
 
 		string[] consoleText=consoleOutput.text.Split('\n');
 
@@ -54,5 +55,50 @@ public class ConsoleController : MonoBehaviour {
 			}
 		}
 
+	}
+
+	void ConsoleCommandParse(string commandString)
+	{
+		string[] commandTokens=commandString.ToUpper().Split(' ');
+
+		switch (commandTokens[0]) 
+		{
+			case "HELP" :
+			{
+				HelpCommand(commandTokens);
+				break;
+			}
+			case "?":
+			{
+				HelpCommand(commandTokens);
+				break;
+			}
+			default:
+			{
+				ConsoleOutputAdd("- Invalid Command: "+commandTokens[0]);
+				break;
+			}
+
+		}
+	}
+
+	void HelpCommand(string[] splitCommand)
+	{
+		if (splitCommand.Length==1) {
+			TextAsset helpFile=Resources.Load("HelpCommand") as TextAsset;
+			ConsoleOutputAdd(helpFile.text);
+		}
+		else 
+		{
+			switch (splitCommand[1])
+			{
+			case "HELP":
+				ConsoleOutputAdd("- Type \"Help\" or \"?\" to see a list of commands");
+				break;
+			default:
+				ConsoleOutputAdd("- Could not find help for '"+splitCommand[1]+"'");
+				break;
+			}
+		}
 	}
 }
