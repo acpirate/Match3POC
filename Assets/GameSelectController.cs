90f3d74d876ad2c70eeb99a46f427027153bcd89
acpirate <3apx3zss29@snkmail.com>
2015-05-21 13:57:15 -0400

    score display on game select screen

---
+++ Assets/GameSelectController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameSelectController : MonoBehaviour {
+
+	public Text scoreDisplay;
+	public Text highScoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+
+		scoreDisplay.text="Last Score:"+GameController.score.ToString();
+		highScoreDisplay.text="High Score:"+GameController.highScore.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
